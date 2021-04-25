fd43fa7e2a34c05a3b888b3fb5e300bd55622080
Samantha Ryan-Lee <sryanlee@umich.edu>
2021-04-25 16:00:58 -0400

    parsed commands, debugging

--- proj3_choc.py
+++ proj3_choc.py
@@ -12,16 +12,19 @@ import sqlite3
 DBNAME = 'choc.sqlite'
 conn = sqlite3.connect(DBNAME)
 cur = conn.cursor()
-bars_select = """b.SpecificBeanBarName,
-                    b.Company,
-                    c.EnglishName,
-                    b.Rating,
-                    b.CocoaPercent,
-                    c.Region""" # CompanyLocation and BroadBeanOrigin are FK on Countries Table
+# CompanyLocation and BroadBeanOrigin are FK on Countries Table
 
 class Query():
-    def __init__(self, high_level_command='bars', select=bars_select, join='CompanyLocationId', where_type=None, where_filter=None, sort='AVG(b.Rating)', direction='desc', limit=10):
-        self.command = high_level_command
+    def __init__(self, hlc='bars',
+                select="b.SpecificBeanBarName, b.Company, c.EnglishName, b.Rating, b.CocoaPercent, c.Region",
+                join='CompanyLocationId',
+                where_type=None,
+                where_filter=None,
+                sort='b.Rating',
+                direction='desc',
+                limit=10):
+
+        self.command = hlc
         self.select = select
         self.join = join
         self.where_type = where_type
@@ -30,31 +33,33 @@ class Query():
         self.direction = direction
         self.limit = limit
 
-    def query_default_results(self):
-        default_query = (
-        f"Select {self.select} from Bars b"
-        f" join Countries c on c.id=b.{self.join}"
-        f" order by {self.sort} {self.direction}"
-        f" limit {self.limit};"
+    def print_attributes(self):
+        return print(
+            f"command: {self.command}\nselect: {self.select}\njoin: {self.join}\nwhere_type: {self.where_type}\nwhere_filter: {self.where_filter}\nsort: {self.sort}\ndirection: {self.direction}\nlimit: {self.limit}"
         )
-        results = cur.execute(default_query).fetchall()
-        return results
 
-    def query_filtered_results(self):
-        if self.where_type is not None and self.where_filter is not None:
-            country_filtered_query = (
+    def print_query(self):
+        if self.where_type is not None:
+            filtered_query = (
                     f"Select {self.select} from Bars b"
                     f" join Countries c on c.id=b.{self.join}"
                     f" where {where_type} like '{where_filter}'"
                     f" order by {self.sort} {self.direction}"
                     f" limit {self.limit};"
                 )
-            results = cur.execute(country_filtered_query).fetchall()
-        else:
-            query_default_results(self)
-            results = cur.execute(query_default_results(self)).fetchall()
+            print(filtered_query)
+            #results = cur.execute(country_filtered_query).fetchall()
+        elif self.where_type is None:
+            default_query = (
+                f"Select {self.select} from Bars b"
+                f" join Countries c on c.id=b.{self.join}"
+                f" order by {self.sort} {self.direction}"
+                f" limit {self.limit};"
+            )
+            print(default_query)
+            #results = cur.execute(default_query).fetchall()
+        return None
 
-        return results
 
 
 # Part 1: Implement logic to process user commands
@@ -89,110 +94,92 @@ def parse_command(command):
     where_type_options = ['country', 'region']
     sort_options = ['ratings', 'cocoa', 'number_of_bars']
     direction_options = ['top', 'bottom']
+    digits = ['0', '1', '2', '3', '4', '5', '6','7', '8','9']
     query_input_list = command.split(' ')
+    query_attribs = {}
 
-    if len(command) == 6:
-        if query_input_list[0] in hlc_options:
-            hlc = query_input_list[0]
-            select = hlc + '_select'
-        else:
-            print("enter a valid high level command")
-
-        if query_input_list[1] in join_options:
-            if query_input_list[1] == 'source':
+    for item in query_input_list:
+        if item in hlc_options:
+            hlc = item
+            query_attribs['hlc'] = hlc
+        elif item in join_options:
+            if item == 'source':
                 join = "BroadBeanOriginId"
+                query_attribs['join'] = join
             else:
                 join = 'CompanyLocationId'
-        else:
-            print("enter a valid join parameter")
-
-        if '=' in query_input_list[2]:
-            where = query_input_list[2].split()
+                query_attribs['join'] = join
+        elif '=' in item:
+            where = item.split('=')
             where_type = where[0]
             where_filter = where[1]
             if where_type == 'country':
                 where_type = 'c.Alpha2'
+                query_attribs['where_type'] = where_type
+                query_attribs['where_filter'] = where_filter
             elif where_type == 'region':
                 where_type = 'c.Region'
-        else:
-            where_type = None
-            where_filter = None
-            print("No where parameter")
-
-        if query_input_list[3] in sort_options:
-            if query_input_list[3] == 'cocoa':
+                query_attribs['where_type'] = where_type
+                query_attribs['where_filter'] = where_filter
+        elif item in sort_options:
+            if item == 'cocoa':
                 sort = 'AVG(b.CocoaPercent)'
-            elif query_input_list[3] == 'number_of_bars':
+                #query_attribs['sort'] = sort
+            elif item == 'number_of_bars':
                 sort = 'COUNT(b.SpecificBeanBarName)'
+                #query_attribs['sort'] = sort
             else:
                 sort = 'AVG(b.Rating)'
-        else:
-            print("enter a valid sort parameter")
-
-        if query_input_list[4] in direction_options:
-            if query_input_list[4] == 'bottom':
+                #query_attribs['sort'] = sort
+        elif item in direction_options:
+            if item == 'bottom':
                 direction = 'asc'
+                query_attribs['direction'] = direction
             else:
                 direction = 'desc'
-        else:
-            print("enter a valid direction paramter")
+                query_attribs['direction'] = direction
+        elif item in range(1,999):
+            limit = int(item)
+            query_attribs['limit'] = limit
 
-        if query_input_list[5] is int:
-            limit = query_input_list[5]
-        else:
-            print("enter a valid limit parameter")
+    if hlc == 'regions':
+        select = f'c.Region, {sort}'
+    elif hlc == 'companies':
+        select = f'b.Company, c.EnglishName, {sort}'
+    elif hlc == 'countries':
+        select = f'c.EnglishName, c.Region, {sort}'
     else:
-        print('enter a valid command')
+        if sort == 'AVG(b.CocoaPercent)':
+            sort = 'b.CocoaPercent'
+            select = f"b.SpecificBeanBarName, b.Company, c.EnglishName, b.Rating, {sort}, c.Region"
+        elif sort == 'COUNT(b.SpecificBeanBarName)':
+            sort = 'b.SpecificBeanBarName'
+            select = f"{sort}, b.Company, c.EnglishName, b.Rating, b.CocoaPercent, c.Region"
+        else:
+            sort = 'b.Rating'
+            select = f"b.SpecificBeanBarName, b.Company, c.EnglishName, {sort}, b.CocoaPercent, c.Region"
+    query_attribs['select'] = select
+    query_attribs['sort'] = sort
 
-        query_attribs = {'hlc': hlc,
-                        'select': select,
-                        'join': join,
-                        'where_type': where_type,
-                        'where_filter': where_filter,
-                        'sort': sort,
-                        'direction': direction,
-                        'limit': limit}
     return query_attribs
 
 
 def create_Query_instance(query_attribs):
-    command_instance = Query(
-        high_level_command=query_attribs['hlc'],
-        select=query_attribs['select'],
-        join=query_attribs['join'],
-        where_type=query_attribs['where_type'],
-        where_filter=query_attribs['where_filter'],
-        sort=query_attribs['sort'],
-        direction=query_attribs['direction'],
-        limit=query_attribs['limit']
-    )
+    command_instance = Query(query_attribs)
     return command_instance
 
 
 # Make sure nothing runs or prints out when this file is run as a module/library
 if __name__=="__main__":
-    default = Query()
-
-    companies_select = f'b.Company, c.EnglishName, {default.sort}'
-    countries_select = f'c.EnglishName, c.Region, {default.sort}'
-    regions_select = f'c.Region, {default.sort}'
-
-    companies_default = Query(high_level_command='companies', select=companies_select)
-    countries_default = Query(high_level_command='countries', select=countries_select)
-    regions_default = Query(high_level_command='regions', select=regions_select)
-
-    default_results = default.query_default_results()
-    default_companies_results = companies_default.query_default_results()
-    default_countries_results = countries_default.query_default_results()
-    default_regions_results = regions_default.query_default_results()
-
     example_bars_command = 'bars sell region=Europe cocoa bottom 5'
-    example_query_input_list = parse_command(example_bars_command)
-    print(query_input_list)
-
-
-
-
+    print(example_bars_command)
+    print('------------------------------')
+    example_query_input_dict = parse_command(example_bars_command)
+    print(example_query_input_dict)
+    print('------------------------------')
+    example_query_instance = create_Query_instance(example_query_input_dict)
+    example_query_instance.print_attributes()
+    example_query_instance.print_query()
 
     #interactive_prompt()
 
